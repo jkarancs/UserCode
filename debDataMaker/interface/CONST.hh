fd87e60a9fca72cdb1a3ca068ca2eb0704fae59f
veszpv <>
2009-06-08 09:42:34 +0000

    Added a map that translates ROOT::TBranch variable types to their name and size.

--- debDataMaker/interface/CONST.hh
+++ debDataMaker/interface/CONST.hh
@@ -14,6 +14,28 @@
     int const FROM_RECO=1;
     int const FROM_SIM=2;
 
+    std::pair<char,std::pair<std::string,size_t> > _ROOTVariableTypes_[] = {
+      std::pair<char,std::pair<std::string,size_t> >(
+	 'F', std::pair<std::string,size_t>("Float_t", sizeof(Float_t))),
+      std::pair<char,std::pair<std::string,size_t> >(
+	 'D', std::pair<std::string,size_t>("Double_t", sizeof(Double_t))),
+      std::pair<char,std::pair<std::string,size_t> >(
+	 'I', std::pair<std::string,size_t>("Int_t", sizeof(Int_t))),
+      std::pair<char,std::pair<std::string,size_t> >(
+	 'L', std::pair<std::string,size_t>("Long_t", sizeof(Long_t))),
+      std::pair<char,std::pair<std::string,size_t> >(
+	 'i', std::pair<std::string,size_t>("UInt_t", sizeof(UInt_t))),
+      std::pair<char,std::pair<std::string,size_t> >(
+	 'l', std::pair<std::string,size_t>("ULong_t", sizeof(ULong_t)))
+    };
+    
+   std::map<char,std::pair<std::string,size_t> > 
+   ROOTVariableTypeMap(_ROOTVariableTypes_,
+		       _ROOTVariableTypes_+
+		       sizeof(_ROOTVariableTypes_)/
+		       sizeof(_ROOTVariableTypes_[0]));
+
+
   }
 
 #endif
