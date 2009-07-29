269fc4b73b40fe56ad349e5cfb8e5ad8eadfcf7a
veszpv <>
2009-07-29 13:31:49 +0000

    Modified Container so that it inherits from any stl container.

--- debDataMaker/interface/Container.hh
+++ debDataMaker/interface/Container.hh
@@ -81,7 +81,7 @@
 //
 // Original Author:  Viktor VESZPREMI
 //         Created:  Wed Mar 18 10:28:26 CET 2009
-// $Id: Container.hh,v 1.1 2009/07/16 17:14:59 veszpv Exp $
+// $Id: Container.hh,v 1.2 2009/07/17 13:12:41 veszpv Exp $
 //
 //
 //-----------------------------------------------------------------------------
@@ -95,59 +95,78 @@ namespace deb {
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 
-template <class D> class Container : public std::vector<D> {
+template <class C, class D, class K=size_t> class Container : public C {
  public:
   Container(std::string name="") : name_(name) { init(0); }
   Container(std::string name, size_t storeNObjects) : name_(name) { 
     init(storeNObjects);
   }
-  virtual ~Container() { this->clear(); }
-  
- private:
-  std::string      name_;
-  size_t           storeNObjects_;     // storeNObjects after calling select()
-  std::string      selectionType_;     // 1, 2,... variants of data selection.
-  bool             valid_;             // tells if object is valid
-  std::map<std::string,std::pair<char,size_t> > vars_;
-  D                objVoid_;
+  Container(std::string name, std::vector<K>& storeList) : name_(name) {
+    init(storeList);
+  }
+  virtual          ~Container() { this->clear(); }
+
+  typedef C                 StlType;
+  typedef D                 ValueType;
+  typedef K                 KeyType;
+  typedef std::vector<K>    KeyList;
 
  protected:
-  TTree*           tree_;              // pointer to tree if being saved there
+  std::string       name_;
+  size_t            storeNObjects_;     // storeNObjects after calling select()
+  std::vector<K>    storeList_;
+  std::string       selectionType_;     // 1, 2,... variants of data selection.
+  bool              valid_;             // tells if object is valid
+  std::map<std::string,std::pair<char,size_t> > vars_;
+  D                 objVoid_;
+
+ protected:
+  TTree*            tree_;              // pointer to tree if being saved there
 
  private:
-  void             init(size_t);
+  void              init(size_t);
+  void              init(std::vector<K>&);
 
  public:
-  inline D&        operator ()(size_t i) { return this->at(i); }
-  inline D*        data(size_t i) { return &this->at(i); }
-  inline size_t    storeNObjects() { return storeNObjects_; }
-  std::string      name() { return name_; }
-  void             setName(const char *name) { name_=name; }
-  void             setSelectionType(std::string type) { selectionType_=type; }
-  std::string      getSelectionType() { return selectionType_; }
-  void             setValid(bool choice) { valid_=choice; }
-  bool             isValid() { return valid_; }
-  std::string      list(std::string prefix="") { return objVoid_.list(prefix);}
+  //virtual std::string       keyToString(K)=0;
+  virtual KeyType   key(typename C::iterator)=0;
+  virtual D*        operator ()(K i) { return &(*this)[i]; } // Need to check i
+  virtual D*        data(size_t i) { return NULL; }
+  inline size_t     storeNObjects() { return storeNObjects_; }
+  const std::vector<K>& storeList() { return storeList_; }
+  std::string       name() { return name_; }
+  void              setName(const char *name) { name_=name; }
+  void              setSelectionType(std::string type) { selectionType_=type; }
+  std::string       getSelectionType() { return selectionType_; }
+  void              setValid(bool choice) { valid_=choice; }
+  bool              isValid() { return valid_; }
+  std::string       list(std::string prefix="") {return objVoid_.list(prefix);}
 
-  void             addBranch(TTree*);
-  void             setBranch();
-  void             stdErr(std::string, ...);
-  void             stdWarn(std::string, ...);
-  void             stdMesg(std::string, ...);
-  void             print(int);
-  void             select();
+  void              addBranch(TTree*);
+  void              setBranch();
+  void              stdErr(std::string, ...);
+  void              stdWarn(std::string, ...);
+  void              stdMesg(std::string, ...);
+  void              print(int);
 
   //
   // functions that need to be implemented in inheriting class
   // ---------------------------------------------------------
 
-  virtual int      passed(std::string, size_t);
+  virtual void      calculate() { 
+    this->stdWarn("calculate(): virtual function is not implemented\n");
+  }
+  void              calculate_pass();
+  void              select();
+  virtual int       passed(std::string, typename C::const_iterator);
+  virtual int       passed(std::string, size_t);
 
 };
 
 //--------------------------------- Constructor -------------------------------
 
-template <class D> void Container<D>::init(size_t storeNObjects){
+template <class C, class D, class K> 
+void Container<C,D,K>::init(size_t storeNObjects) {
   this->clear();
   storeNObjects_=storeNObjects;
   setSelectionType(NOVAL_S);
@@ -156,45 +175,82 @@ template <class D> void Container<D>::init(size_t storeNObjects){
   tree_=NULL;
 }
 
+template <class C, class D, class K>
+void Container<C,D,K>::init(std::vector<K>& storeList){
+  storeList_.clear();
+  typename std::vector<K>::const_iterator it;
+  for (it=storeList.begin(); it!=storeList.end(); it++) {
+    typename std::vector<K>::const_iterator it2;
+    it2=std::find(storeList_.begin(), storeList_.end(), *it);
+    if (it2==storeList_.end()) {
+      storeList_.push_back(*it);
+    }
+  }
+  init(storeList_.size());
+}
+
+// //------------------------------- keyToString() -------------------------------
+
+// template <class C, class D, class K> 
+// std::string Container<C,D,K>::keyToString(K key) {
+//   std::ostringstream ss;
+//   ss<<key;
+//   return ss.str();
+// }
+
 //--------------------------------- addBranch() -------------------------------
 
-template <class D> void Container<D>::addBranch(TTree* tree){
+template <class C, class D, class K> 
+void Container<C,D,K>::addBranch(TTree* tree){
   if (!isValid()) {
     stdErr("addBranch(): Object is not valid, cannot add it to tree\n");
     return;
   }
+  if (storeNObjects_==0) return;
   if (tree_!=NULL) {
     stdWarn("addBranch(): Object is already added to a tree.\n");
     return;
   }
   tree_=tree;
-  if (storeNObjects_==1) {
+
+  if (storeNObjects_==1 && storeList_.size()==0) {
     tree_->Branch(name_.data(), &objVoid_, objVoid_.list().data());
     return;
   }
+
   for (size_t i=0; i<storeNObjects_; i++) {
     std::ostringstream ss;
-    ss<<name_<<"_"<<i;
+    if (storeList_.size()==0) {
+      ss<<name_<<"_"<<i;
+    } else {
+      ss<<name_<<"_"<<keyToString(storeList_[i]);
+    }
     tree_->Branch(ss.str().data(), &objVoid_, objVoid_.list().data());
   }
 }
 
+
 //--------------------------------- setBranch() -------------------------------
 
-template <class D> void Container<D>::setBranch() {
+template <class C, class D, class K> 
+void Container<C,D,K>::setBranch() {
   if (!isValid()) {
     stdErr("setBranch(): Object is not valid, can't fill its branches.\n");
     return;
   }
+  if (storeNObjects_==0) return;
   if (tree_==NULL) {
     stdErr("setBranch(): Object hasn't been added to tree. "	\
 	   "(Do it in the constructor!)\n");
     return;
   }
-  if (storeNObjects_==1) {
+
+  objVoid_.clear();
+  typename C::iterator it=this->begin();
+  if (storeNObjects_==1 && storeList_.size()==0) {
     TBranch* br=tree_->GetBranch(name_.data());
     if (br!=NULL) {
-      if (this->size()!=0) br->SetAddress(data(0));
+      if (this->size()!=0) br->SetAddress(&(*it));
       else br->SetAddress(&objVoid_);
     }
     else stdErr("setBranch(): Did not find branch %s in tree!!!\n", 
@@ -203,20 +259,42 @@ template <class D> void Container<D>::setBranch() {
   }
   for (size_t i=0; i<storeNObjects_; i++) {
     std::ostringstream ss;
-    ss<<name_<<"_"<<i;
+    if (storeList_.size()==0) {
+      ss<<name_<<"_"<<i;
+    } else {
+      ss<<name_<<"_"<<keyToString(storeList_[i]);
+    }
     TBranch* br=tree_->GetBranch(ss.str().data());
     if (br!=NULL) {
-      if (i<this->size()) br->SetAddress(data(i));
-      else br->SetAddress(&objVoid_);
+      if (i<this->size()) {
+	if (storeList_.size()==0) {
+	  br->SetAddress(&(*it));	  
+	} else {
+	  D* data=(*this)(storeList_[i]);
+	  if (data==NULL) {
+	    stdErr("setBranch(): data with key %s does not exist",
+		   keyToString(storeList_[i]).data());
+	    br->SetAddress(&objVoid_);
+	  } else {
+	    br->SetAddress(data);
+	  }
+	  //br->SetAddress(&(*this)[storeList_[i]]);
+	}
+      } else {
+	br->SetAddress(&objVoid_);
+      }
     }
     else stdErr("setBranch(): Did not find branch %s in tree!!!\n", 
 		ss.str().data());
+    it++;
   }
 }
 
+
 //--------------------------------- stdMesg() ---------------------------------
  
-template <class D> void Container<D>::stdErr(std::string mesg, ...) {
+template <class C, class D, class K> 
+void Container<C,D,K>::stdErr(std::string mesg, ...) {
   va_list argList;
   va_start(argList, mesg);
   char s[256];
@@ -227,7 +305,9 @@ template <class D> void Container<D>::stdErr(std::string mesg, ...) {
 	   <<mesg<<std::endl;
 }
 
-template <class D> void Container<D>::stdWarn(std::string mesg, ...) {
+
+template <class C, class D, class K> 
+void Container<C,D,K>::stdWarn(std::string mesg, ...) {
   va_list argList;
   va_start(argList, mesg);
   char s[256];
@@ -238,7 +318,9 @@ template <class D> void Container<D>::stdWarn(std::string mesg, ...) {
 	   <<std::endl;
 }
 
-template <class D> void Container<D>::stdMesg(std::string mesg, ...) {
+
+template <class C, class D, class K> 
+void Container<C,D,K>::stdMesg(std::string mesg, ...) {
   va_list argList;
   va_start(argList, mesg);
   char s[256];
@@ -250,23 +332,50 @@ template <class D> void Container<D>::stdMesg(std::string mesg, ...) {
 
 //---------------------------------- print() ----------------------------------
 
-template <class D> void Container<D>::print(int verbose=0) {
+template <class C, class D, class K> 
+void Container<C,D,K>::print(int verbose=0) {
   stdMesg("  Number of valid objects in container (size): %d", this->size());
   stdMesg("  Number of objects to be stored (storeNObjects): %d", 
 	  storeNObjects());
+  if (storeList_.size()!=0) {
+    std::ostringstream ss; 
+    ss<<keyToString(storeList_[0]);
+    for (size_t i=1; i<storeList_.size(); i++) 
+      ss<<", "<<keyToString(storeList_[i]);
+    stdMesg("   '%s'", ss.str().data());
+  }
   stdMesg("  Selection criteria for select() function (selectionType_) : %s",
 	  selectionType_.data());
   stdMesg("  List of variables: %s", objVoid_.list().data());
   stdMesg("  Object is %svalid!\n", (isValid() ? "" : "not "));
-  if (verbose<1) return;
+  if (verbose<1) {
+    stdMesg("");
+    return;
+  }
 
   if (tree_!=NULL) {
     stdMesg("  Added to tree '%s' as branch '%s_X'", tree_->GetName(), 
 	    name_.data());
   }
   stdMesg("  Container map:");
-  for (size_t i=0; i<this->size(); i++) {
-    stdMesg("   (%d): 0x%x - 0x%x", i, data(i), data(i)+sizeof(D));
+  //  for (size_t i=0; i<this->size(); i++) {
+  typename C::iterator iObj=this->begin();
+  for (size_t i=0; iObj!=this->end(); iObj++) {
+    std::string sstore="";
+    std::string k=keyToString(key(iObj));
+    if (k.find_first_of("_")!=std::string::npos) 
+      k.replace(k.find_first_of("_"), 1, ",");
+    if (storeNObjects()!=0) {
+      if (storeList_.size()==0) {
+	if (i<storeNObjects()) sstore=" * [STORE]";
+      } else {
+	size_t j=0;
+	for (; j<storeList_.size(); j++) if (storeList_[j]==key(iObj)) break;
+	if (j!=storeList_.size()) sstore=" * [STORE]";
+      }
+    }
+    stdMesg("   (%s): 0x%x - 0x%x %s", 
+	    k.data(), &(*iObj), &(*iObj)+sizeof(D), sstore.data());
     if (verbose<2) continue;
     std::map<std::string,std::pair<char,size_t> >::const_iterator it;
     for (it=vars_.begin(); it!=vars_.end(); it++) {
@@ -276,7 +385,9 @@ template <class D> void Container<D>::print(int verbose=0) {
       if ((*it).second.first=='F' || (*it).second.first=='D') ss<<"%f";
       else if ((*it).second.first=='I'||(*it).second.first=='L') ss<<"%d";
       else if ((*it).second.first=='i'||(*it).second.first=='l') ss<<"%u";
-      void* pv=(void *)data(i);
+      //      void* pv=(void *)data(i);
+      //void* pv=(void *)(&(*iObj));
+      void* pv=(void *)(&(*this)[key(iObj)]);
       char *p=(char *)pv+(*it).second.second;
       if ((*it).second.first=='F') stdMesg(ss.str(), *(float*)p);
       else if((*it).second.first=='D') stdMesg(ss.str(), *(double*)p);
@@ -285,12 +396,46 @@ template <class D> void Container<D>::print(int verbose=0) {
       else if((*it).second.first=='i') stdMesg(ss.str(), *(unsigned int*)p);
       else if((*it).second.first=='l') stdMesg(ss.str(), *(unsigned long*)p);
     }
+    i++;
+  }
+  stdMesg("");
+}
+
+//------------------------ calculate_pass() -----------------------------------
+
+template <class C, class D, class K> void Container<C,D,K>::calculate_pass() {
+  if (!this->isValid()) return;
+  for (size_t i=0; i<this->size(); i++) {
+    this->at(i).pass=0;
+    std::map<std::string, int>::const_iterator it;
+    for (it=this->at(i).selectionTypes_.begin();
+	 it!=this->at(i).selectionTypes_.end();
+	 it++){
+      if (it->first.find("VALID")!=std::string::npos) {
+	this->at(i).pass|=1<<it->second;
+	continue;
+      }
+      int result=this->passed(it->first, i);
+      if (result!=0 && result!=1) {
+	this->stdErr("calculate_pass(): selection %s on element %d returned " \
+		     "value %d", it->first.data(), i, result);
+	this->stdErr("...is it implemented? ...are all the parameters "	\
+		     "required for this selection computed by the time " \
+		     "calculate_pass is called?\n");
+	this->print(3);
+	this->at(i).pass=0;
+	return;
+      }
+      this->at(i).pass|=result<<it->second;
+    }
   }
 }
 
+
 //--------------------------------- select() ----------------------------------
 
-template <class D> void Container<D>::select() {
+template <class C, class D, class K> void Container<C,D,K>::select() {
+
   if (!isValid()) return;
   if (selectionType_==NOVAL_S) return;
   if (objVoid_.selectionTypes_.find(selectionType_) ==
@@ -300,16 +445,26 @@ template <class D> void Container<D>::select() {
 	   selectionType_.data());
     return;
   }
-  for (int i=0; i<long(this->size()); i++) {
-    if (passed(selectionType_, i)!=0) continue; 
-    this->erase(this->begin()+i);
-    --i;
+  for(typename C::reverse_iterator rit=this->rbegin(); rit!=this->rend();) {
+    rit++;
+    typename C::iterator erase_it=rit.base();
+    if (passed(selectionType_, erase_it)==0) this->erase(erase_it);
   }
 }
 
+
 //--------------------------------- passed() ----------------------------------
 
-template <class D> int Container<D>::passed(std::string selection, size_t i) {
+template <class C, class D, class K>
+int Container<C,D,K>::passed(std::string selection, 
+			      typename C::const_iterator it) {
+  stdErr("passed(): virtual function passed() has not been implemented\n");
+  return NOVAL_I;
+}
+
+
+template <class C, class D, class K>
+int Container<C,D,K>::passed(std::string selection, size_t i) {
   stdErr("passed(): virtual function passed() has not been implemented\n");
   return NOVAL_I;
 }
