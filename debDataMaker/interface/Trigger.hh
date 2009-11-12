3b00c7f6084fbc56b4c3631e826dea50fa9f5eed
aranyi <>
2009-11-12 14:40:19 +0000

    inherited from VContainer

--- debDataMaker/interface/Trigger.hh
+++ debDataMaker/interface/Trigger.hh
@@ -36,105 +36,32 @@
 //
 // Original Author:  Anita KAPUSI
 //         Created:  Wed Mar 18 10:28:26 CET 2009
-// $Id: Trigger.hh,v 1.2 2009/06/05 19:59:25 veszpv Exp $
+// $Id: Trigger.hh,v 1.3 2009/06/15 17:19:42 veszpv Exp $
 //
 //
 //-----------------------------------------------------------------------------
 
-#include "DataFormats/Common/interface/View.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "SusyAnalysis/debDataMaker/interface/Data.hh"
+#include "SusyAnalysis/debDataMaker/interface/MContainer.hh"
 #include "SusyAnalysis/debDataMaker/interface/TriggerData.hh"
 
 namespace deb {
 
 //----------------------------- Class Definition ------------------------------
 
-template<class T> class Trigger : public Data<TriggerData>{ 
-                                                          // D:=TriggerData
- public:
-  Trigger(const edm::ParameterSet& iConfig);
-  Trigger() { stdErr("  Trigger<%s> configuration missing\n", 
-		     typeid(T).name()); }
-  inline TriggerData& trigger(unsigned int i) { return *data(i); } 
-                                                          // just a short-hand
+class Trigger : public MContainer<std::string,TriggerData> {
+  public:
+    Trigger(std::string name="") : MContainer<std::string,TriggerData>(name) { }
+    Trigger(std::string name, std::vector<std::string> storeList)
+  : MContainer<std::string, TriggerData>(name, storeList) { }
+    ~Trigger() { }
+
+    inline TriggerData& trigger(std::string triggerName) { return *(*this)(triggerName); }
 
   // Inherited functions to be overloaded
-  void set(const edm::Event&);
-  void calculate ();
-  int passed(std::string selection);
+    void calculate () { };
+    int passed(std::string selection);
 
-  // Introduce new variables and functions
- private:
-  edm::InputTag triggerResults_;        // tag for input collection
-  std::vector<std::string> pathNames_;  // trigger path names (ORed)  
-  
 };
-
-
-//--------------------------------- Constructor -------------------------------
-/*
-   List of parameters to gear the object with (passed in iConfig):
-      edm::InputTag triggerResults_,       : owned by this class
-      std::vector<std::string> pathNames_, : owned by this class      
-*/
-
-template<class T> Trigger<T>::Trigger(const edm::ParameterSet& iConfig) : 
-  Data<TriggerData>(int(1)) {
-  
-  triggerResults_= iConfig.getParameter<edm::InputTag>("triggerResults");
-  pathNames_= iConfig.getParameter< std::vector<std::string> >("pathNames");
-
-  stdMesg("  Trigger<%s> configuration:", typeid(T).name());
-  stdMesg("\ttriggerResults = %s", triggerResults_.label().data());
-  for (unsigned int i=0; i<pathNames_.size(); i++){
-    stdMesg("\tpathNames_[%d] = %s", i, pathNames_[i].c_str());  
-  }
-  stdMesg("  List of variables: %s\n", trigger(0).list().data());
-
-}
-
-//----------------------------------- set() -----------------------------------
-
-template<class T> void Trigger<T>::set(const edm::Event& iEvent) {
-
-  if (!isValid()) return;
-
-  edm::Handle<T> hltHandle;
-  iEvent.getByLabel(triggerResults_, hltHandle);
-
-  clear();
-  TriggerData new_obj;
-  push_back(new_obj);
-  if ( hltHandle.isValid() ) {
-    edm::TriggerNames trgNames;
-    trgNames.init(*hltHandle);
-    for ( unsigned int i=0; i<pathNames_.size(); ++i ) {
-      unsigned int index = trgNames.triggerIndex(pathNames_[i]);
-      if ( index==trgNames.size() ) {
-	stdErr("set(): Unknown trigger name: %s\n", pathNames_[i].c_str());
-	continue;
-      }
-    } 
-    trigger(0).hlt = hltHandle->accept();
-  }
-  else {  
-    stdErr("set(): No trigger results for InputTag\n");
-  }
-
-}
-  
-//-------------------------------- calculate() --------------------------------
-
-template<class T> void Trigger<T>::calculate () { 
-}
-  
-//--------------------------------- passed() ----------------------------------
-
-template<class T> int Trigger<T>::passed(std::string selection) { 
-  return 1; 
-}
-
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
