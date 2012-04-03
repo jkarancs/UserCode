851ed1da9c620c159b524ecb2cb000a2b65a52e6
jkarancs <>
2012-04-03 11:38:46 +0000

    *** empty log message ***

--- PixelTimingStudy/test/MB_RECO_cfg.py
+++ PixelTimingStudy/test/MB_RECO_cfg.py
@@ -221,7 +221,7 @@ process.source = cms.Source("PoolSource",
 #-------------------------------------------------
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-0))
+    input = cms.untracked.int32(-1))
 
 
 #-------------------------------------------------
